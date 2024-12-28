ce500fdc92a478e05ba26f70005bf2d7e20a5fc6
Manoj Kamatam <128217010+ManojKamatam@users.noreply.github.com>
2024-12-28 23:57:23 +0530

    Update restart_server.sh

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Restarting Gunicorn server..."
-systemctl restart gunicorn
+echo "Restarting Gunicorn server..." >> /var/log/codedeploy_restart.log
+sudo systemctl restart gunicorn >> /var/log/codedeploy_restart.log 2>&1
