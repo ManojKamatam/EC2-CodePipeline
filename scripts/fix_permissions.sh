67209ed3a0f0b44643cc7734d016ec18eb0e4efd
Manoj Kamatam <128217010+ManojKamatam@users.noreply.github.com>
2024-12-29 12:37:49 +0530

    Update fix_permissions.sh

--- scripts/fix_permissions.sh
+++ scripts/fix_permissions.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-
-# Find the correct deployment directory dynamically
 DEPLOY_DIR=$(find /opt/codedeploy-agent/deployment-root/ -name "deployment-archive" -type d | head -n 1)
-
 if [ -z "$DEPLOY_DIR" ]; then
-  echo "Deployment directory not found."
-  exit 1
+    echo "Deployment directory not found."
+    exit 1
 fi
 
-echo "Deployment directory found: $DEPLOY_DIR"
-
-# Make all scripts executable
 chmod +x "$DEPLOY_DIR/scripts/"*.sh
